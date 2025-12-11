fb3be501f067dc2be2f08502382a011ecb4b86aa
Monserrattorress <20243ds147@utez.edu.mx>
2025-12-11 11:50:07 -0600

    modifique locationPoint

--- Integradora_TripCam/app/src/main/java/com/example/integradora_tripcam/data/db/LocationPoint.kt
+++ Integradora_TripCam/app/src/main/java/com/example/integradora_tripcam/data/db/LocationPoint.kt
@@ -1,4 +1,24 @@
 package com.example.integradora_tripcam.data.db
 
-class LocationPoint {
-}
+package mx.edu.utez.gps.data.db
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+@Entity(
+    tableName = "location_points",
+    primaryKeys = ["tripId", "timestamp"], // Clave única
+    foreignKeys = [
+        ForeignKey(
+            entity = Trip::class,
+            parentColumns = ["id"],
+            childColumns = ["tripId"],
+            onDelete = ForeignKey.CASCADE // Si borras un Trip, se borran sus puntos
+        )
+    ]
+)
+data class LocationPoint(
+    val tripId: Long,
+    val latitude: Double,
+    val longitude: Double,
+    val timestamp: Long
+)
